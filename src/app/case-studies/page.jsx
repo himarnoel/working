cc14b14bc99cc3bf761f0d54852be1264e963b8a
phantomcancode <vodumeh@gmail.com>
2024-11-26 21:14:28 +0100

    case studies page completed

--- src/app/case-studies/page.jsx
+++ src/app/case-studies/page.jsx
@@ -13,12 +13,12 @@ const page = () => {
             <div className='max-w-[1200px] mx-auto grid grid-cols-1 md:grid-cols-2 justify-items-center place-items-center gap-x-[32px] gap-y-[104px]'>
                 {
                     cases.map((caseItem, index) => (
-                        <div key={index} className='bg-white w-full xl:w-[584px] h-[516px] pb-[32px] gap-[32px] flex flex-col justify-center items-start rounded-bl-[16px] rounded-br-[16px]'>
+                        <div key={index} className='bg-white w-full xl:w-[584px] h-[516px] pb-[16px] md:pb-[32px] gap-[32px] flex flex-col justify-center items-start rounded-bl-[16px] rounded-br-[16px]'>
                             <Image width={584} height={280} className='rounded-tl-[16px] h-[280px] w-full rounded-tr-[16px] object-cover' src={caseItem.img} alt={caseItem.title}></Image>
-                            <div className='px-6 gap-[16px] flex flex-col justify-center items-start w-full'>
-                                <h1 className='text-blue leading-[31.2px] font-instrument text-[1.45rem] font-semibold'>{caseItem.title}</h1>
-                                <p className='text-blueThree leading-[24px] font-instrument text-[1rem] font-normal'>{caseItem.text}</p>
-                                <Link href={`/case-studies/1`} className='text-greenTwo flex justify-center items-center gap-[8px] leading-[24px] text-[1rem] font-medium font-instrument'>
+                            <div className='px-4 md:px-6 gap-[16px] flex flex-col border border-gray justify-center items-start w-full'>
+                                <h1 className='text-blue leading-[23.4px] lg:leading-[31.2px] font-instrument text-[1.125rem] lg:text-[1.45rem] font-semibold'>{caseItem.title}</h1>
+                                <p className='text-blueThree leading-[16.8px] lg:leading-[24px] font-instrument text-[.75rem] lg:text-[1rem] font-normal'>{caseItem.text}</p>
+                                <Link href={`/case-studies/1`} className='text-greenTwo flex justify-center items-center gap-[8px] leading-[24px] text-[.875rem] lg:text-[1rem] font-medium font-instrument'>
                                     Read post
                                     <MdOutlineArrowOutward />
                                 </Link>
