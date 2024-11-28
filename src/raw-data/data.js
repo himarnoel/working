61cb60419361f1c5dc879bd2fc6459cb41db7ee4
phantomcancode <vodumeh@gmail.com>
2024-11-28 22:36:09 +0100

    our services section of the homepage completed

--- src/raw-data/data.js
+++ src/raw-data/data.js
@@ -1,6 +1,16 @@
 import city from '../../public/assets/case-studies/city.svg';
 import stocks from '../../public/assets/case-studies/stocks.svg';
 
+
+import app from '../../public/assets/home/services/application.svg';
+import cloud from '../../public/assets/home/services/cloud.svg';
+import data from '../../public/assets/home/services/data.svg';
+import pdt from '../../public/assets/home/services/product.svg';
+import mob from '../../public/assets/home/services/mobile.svg';
+import it from '../../public/assets/home/services/it.svg';
+import cc from '../../public/assets/home/services/cloud-c.svg';
+import sec from '../../public/assets/home/services/security.svg';
+
 export const cases = [
     {
         id: 1,
@@ -54,5 +64,57 @@ export const links = [
         id: 5,
         name: 'Case studies',
         route: '/case-studies'
+    },
+]
+
+
+export const services = [
+    {
+        id: 1,
+        name: 'Application Development',
+        route: '',
+        img: app
+    },
+    {
+        id: 2,
+        name: 'Cloud Development',
+        route: '',
+        img: cloud
+    },
+    {
+        id: 3,
+        name: 'Data Architecture',
+        route: '',
+        img: data
+    },
+    {
+        id: 4,
+        name: 'Product Development',
+        route: '',
+        img: pdt
+    },
+    {
+        id: 5,
+        name: 'Mobile Application Development',
+        route: '',
+        img: mob
+    },
+    {
+        id: 6,
+        name: 'IT Consulting',
+        route: '',
+        img: it
+    },
+    {
+        id: 7,
+        name: 'Cloud Computing',
+        route: '',
+        img: cc
+    },
+    {
+        id: 8,
+        name: 'Security',
+        route: '',
+        img: sec
     },
 ]
