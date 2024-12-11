1854c67329911a240b06e94b7465caaaf2e5ddc0
phantomcancode <vodumeh@gmail.com>
2024-12-11 12:33:41 +0100

    responsive errors fixed at career page

--- src/app/career/components/JobOpening.jsx
+++ src/app/career/components/JobOpening.jsx
@@ -8,130 +8,40 @@ import { IoIosArrowForward } from 'react-icons/io';
 
 const JobOpening = () => {
   return (
-    <section className='pt-[120px] pb-[160px] px-[64px] gap-[64px] flex flex-col justify-center items-center bg-grayTwentyThree'>
-        <h1 className='text-blue font-bold leading-[67.2px] text-[3.5rem] font-instrument'>Job Openings</h1>
-        <div className='max-w-[1312px] mx-auto gap-[32px] lg:h-[936px] flex flex-col justify-center items-center'>
-            <div className='shadow-md shadow-grayTwentyFour w-full h-[210px] rounded-[8px] gap-[32px] p-[32px] flex flex-col justify-center items-start bg-white border border-graySixteen'>
-                <div className='w-full flex flex-col justify-start items-start gap-[24px]'>
-                    <div className='w-full flex flex-col md:flex-row justify-between items-center'>
-                        <div className='flex justify-start items-center gap-[16px]'>
-                            <h1 className='text-blue font-bold leading-[33.6px] text-[1.5rem] font-instrument'>Job Title</h1>
-                            <div className='w-[96px] h-[29px] border border-greenTwo bg-greenTwo/[16%] py-[4px] flex justify-center items-center gap-[10px] rounded-[4px] text-greenTwo text-[.875rem] leading-[21px] font-normal font-instrument'>Department</div>
+    <section className='pt-[120px] pb-[120px] lg:pb-[160px] px-[23.5px] lg:px-[64px] gap-[40px] lg:gap-[64px] flex flex-col justify-center items-center bg-grayTwentyThree'>
+        <h1 className='text-blue font-bold leading-[38.4px] lg:leading-[67.2px] text-[2rem] lg:text-[3.5rem] font-instrument'>Job Openings</h1>
+        <div className='max-w-[1312px] mx-auto gap-[28px] lg:gap-[32px] lg:h-[936px] flex flex-col justify-center items-center'>
+            {
+                Array.from({ length: 4 }).map((_, index) => (
+                    <div key={index} className='shadow-md shadow-grayTwentyFour w-[328px] sm:w-full md:h-[210px] rounded-[8px] gap-[32px] p-[32px] flex flex-col justify-center items-start bg-white border border-graySixteen'>
+                        <div className='w-full flex flex-col justify-start items-start gap-[24px]'>
+                            <div className='w-full flex flex-col md:flex-row justify-between items-start md:items-center gap-[12px]'>
+                                <div className='flex justify-start items-center gap-[16px]'>
+                                    <h1 className='text-blue font-bold leading-[28px] md:leading-[33.6px] text-[1.25rem] md:text-[1.5rem] font-instrument'>Job Title</h1>
+                                    <div className='w-[84px] md:w-[96px] h-[26px] md:h-[29px] border border-greenTwo bg-greenTwo/[16%] py-[4px] flex justify-center items-center gap-[10px] rounded-[4px] text-greenTwo text-[.75rem] md:text-[.875rem] leading-[18px] md:leading-[21px] font-normal font-instrument'>Department</div>
+                                </div>
+                                <Link className='gap-[8px] font-instrument flex justify-center items-center h-[24px] w-[112px] font-normal text-[.875rem] leading-[21px] md:text-base text-greenTwo' href={`/career`}>
+                                    Apply Now
+                                    <IoIosArrowForward />
+                                </Link>
+                            </div>
+                            <p className='text-blueTwo font-normal leading-[18px] text-[.75rem] md:text-base font-instrument'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. 
+                            </p>
                         </div>
-                        <Link className='gap-[8px] font-instrument flex justify-center items-center h-[24px] w-[112px] font-normal text-base text-greenTwo' href={`/career`}>
-                            Apply Now
-                            <IoIosArrowForward />
-                        </Link>
-                    </div>
-                    <p className='text-blueTwo font-normal text-base font-instrument'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. 
-                    </p>
-                </div>
-                <div className='gap-[24px] flex justify-start items-center w-[302px]'>
-                    <div className='gap-[12px] flex justify-start items-center'>
-                        <Image className='object-cover' width={32} height={32} src={pin} alt='Pin Icon'></Image>
-                        <p className='text-[1.125rem] leading-[27px] font-normal text-blueTwo font-instrument'>Location</p>
-                    </div>
-                    <div className='gap-[12px] flex justify-start items-center'>
-                        <FiClock className='text-greenTwo text-[1.5rem]' />
-                        <p className='text-[1.125rem] leading-[27px] font-normal text-blueTwo font-instrument'>Contract Type</p>
-                    </div>
-                </div>
-            </div>
-
-            {/* Box 2 */}
-            <div className='shadow-md shadow-grayTwentyFour w-full h-[210px] rounded-[8px] gap-[32px] p-[32px] flex flex-col justify-center items-start bg-white border border-graySixteen'>
-                <div className='w-full flex flex-col justify-start items-start gap-[24px]'>
-                    <div className='w-full flex flex-col md:flex-row justify-between items-center'>
-                        <div className='flex justify-start items-center gap-[16px]'>
-                            <h1 className='text-blue font-bold leading-[33.6px] text-[1.5rem] font-instrument'>Job Title</h1>
-                            <div className='w-[96px] h-[29px] border border-greenTwo bg-greenTwo/[16%] py-[4px] flex justify-center items-center gap-[10px] rounded-[4px] text-greenTwo text-[.875rem] leading-[21px] font-normal font-instrument'>Department</div>
+                        <div className='gap-[24px] flex justify-start items-center md:w-[302px]'>
+                            <div className='gap-[12px] flex justify-start items-center'>
+                                <Image className='object-cover w-[20px] md:w-[32px] h-[20px] md:h-[32px]' src={pin} alt='Pin Icon'></Image>
+                                <p className='text-[.75rem] md:text-[1.125rem] leading-[18px] md:leading-[27px] font-normal text-blueTwo font-instrument'>Location</p>
+                            </div>
+                            <div className='gap-[12px] flex justify-start items-center'>
+                                <FiClock className='text-greenTwo text-[1rem] md:text-[1.5rem]' />
+                                <p className='text-[.75rem] md:text-[1.125rem] leading-[18px] md:leading-[27px] font-normal text-blueTwo font-instrument'>Contract Type</p>
+                            </div>
                         </div>
-                        <Link className='gap-[8px] font-instrument flex justify-center items-center h-[24px] w-[112px] font-normal text-base text-greenTwo' href={`/career`}>
-                            Apply Now
-                            <IoIosArrowForward />
-                        </Link>
                     </div>
-                    <p className='text-blueTwo font-normal text-base font-instrument'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. 
-                    </p>
-                </div>
-                <div className='gap-[24px] flex justify-start items-center w-[302px]'>
-                    <div className='gap-[12px] flex justify-start items-center'>
-                        <Image className='object-cover' width={32} height={32} src={pin} alt='Pin Icon'></Image>
-                        <p className='text-[1.125rem] leading-[27px] font-normal text-blueTwo font-instrument'>Location</p>
-                    </div>
-                    <div className='gap-[12px] flex justify-start items-center'>
-                        <FiClock className='text-greenTwo text-[1.5rem]' />
-                        <p className='text-[1.125rem] leading-[27px] font-normal text-blueTwo font-instrument'>Contract Type</p>
-                    </div>
-                </div>
-            </div>
-
-
-            {/* Box 3 */}
-
-            <div className='shadow-md shadow-grayTwentyFour w-full h-[210px] rounded-[8px] gap-[32px] p-[32px] flex flex-col justify-center items-start bg-white border border-graySixteen'>
-                <div className='w-full flex flex-col justify-start items-start gap-[24px]'>
-                    <div className='w-full flex flex-col md:flex-row justify-between items-center'>
-                        <div className='flex justify-start items-center gap-[16px]'>
-                            <h1 className='text-blue font-bold leading-[33.6px] text-[1.5rem] font-instrument'>Job Title</h1>
-                            <div className='w-[96px] h-[29px] border border-greenTwo bg-greenTwo/[16%] py-[4px] flex justify-center items-center gap-[10px] rounded-[4px] text-greenTwo text-[.875rem] leading-[21px] font-normal font-instrument'>Department</div>
-                        </div>
-                        <Link className='gap-[8px] font-instrument flex justify-center items-center h-[24px] w-[112px] font-normal text-base text-greenTwo' href={`/career`}>
-                            Apply Now
-                            <IoIosArrowForward />
-                        </Link>
-                    </div>
-                    <p className='text-blueTwo font-normal text-base font-instrument'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. 
-                    </p>
-                </div>
-                <div className='gap-[24px] flex justify-start items-center w-[302px]'>
-                    <div className='gap-[12px] flex justify-start items-center'>
-                        <Image className='object-cover' width={32} height={32} src={pin} alt='Pin Icon'></Image>
-                        <p className='text-[1.125rem] leading-[27px] font-normal text-blueTwo font-instrument'>Location</p>
-                    </div>
-                    <div className='gap-[12px] flex justify-start items-center'>
-                        <FiClock className='text-greenTwo text-[1.5rem]' />
-                        <p className='text-[1.125rem] leading-[27px] font-normal text-blueTwo font-instrument'>Contract Type</p>
-                    </div>
-                </div>
-            </div>
-
-
-            {/* Box 4 */}
-
-
-            <div className='shadow-md shadow-grayTwentyFour w-full h-[210px] rounded-[8px] gap-[32px] p-[32px] flex flex-col justify-center items-start bg-white border border-graySixteen'>
-                <div className='w-full flex flex-col justify-start items-start gap-[24px]'>
-                    <div className='w-full flex flex-col md:flex-row justify-between items-center'>
-                        <div className='flex justify-start items-center gap-[16px]'>
-                            <h1 className='text-blue font-bold leading-[33.6px] text-[1.5rem] font-instrument'>Job Title</h1>
-                            <div className='w-[96px] h-[29px] border border-greenTwo bg-greenTwo/[16%] py-[4px] flex justify-center items-center gap-[10px] rounded-[4px] text-greenTwo text-[.875rem] leading-[21px] font-normal font-instrument'>Department</div>
-                        </div>
-                        <Link className='gap-[8px] font-instrument flex justify-center items-center h-[24px] w-[112px] font-normal text-base text-greenTwo' href={`/career`}>
-                            Apply Now
-                            <IoIosArrowForward />
-                        </Link>
-                    </div>
-                    <p className='text-blueTwo font-normal text-base font-instrument'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. 
-                    </p>
-                </div>
-                <div className='gap-[24px] flex justify-start items-center w-[302px]'>
-                    <div className='gap-[12px] flex justify-start items-center'>
-                        <Image className='object-cover' width={32} height={32} src={pin} alt='Pin Icon'></Image>
-                        <p className='text-[1.125rem] leading-[27px] font-normal text-blueTwo font-instrument'>Location</p>
-                    </div>
-                    <div className='gap-[12px] flex justify-start items-center'>
-                        <FiClock className='text-greenTwo text-[1.5rem]' />
-                        <p className='text-[1.125rem] leading-[27px] font-normal text-blueTwo font-instrument'>Contract Type</p>
-                    </div>
-                </div>
-            </div>
-
-            
+                ))
+            }
         </div>
     </section>
   )
--- src/app/career/components/OurValues.jsx
+++ src/app/career/components/OurValues.jsx
@@ -8,33 +8,33 @@ import wKite from '../../../../public/assets/career/white-kite.svg';
 const OurValues = () => {
   return (
     <section className='py-[120px] w-full bg-white min-h-screen gap-[80px] flex flex-col justify-center items-center'>
-        <div className='max-w-[1240px] mx-auto gap-[88px] flex flex-col justify-center items-center'>
-            <div className='w-full flex flex-col justify-center items-center gap-[10px] text-center'>
-                <h1 className='font-bevan font-normal leading-[57.6px] text-[3rem] text-grayTwenty'>Our Values</h1>
-                <p className='text-blueTen leading-[24px] font-normal text-[1rem] w-[760px]'>
+        <div className='max-w-[1240px] mx-auto gap-[40px] lg:gap-[88px] flex flex-col justify-center items-center'>
+            <div className='w-[328px] lg:w-full flex flex-col justify-center items-center gap-[10px] text-center'>
+                <h1 className='font-bevan font-normal leading-[38.4px] lg:leading-[57.6px] text-[2rem] lg:text-[3rem] text-grayTwenty'>Our Values</h1>
+                <p className='text-blueTen leading-[21px] lg:leading-[24px] font-normal text-[.875rem] lg:text-[1rem] lg:w-[760px]'>
                     At Elara Solutions, we value our employees and are dedicated to their well-being and success. We offer a comprehensive range of benefits designed to support their personal and professional growth.
                 </p>
             </div>
-            <div className='w-full gap-y-[64px] gap-x-[40px] grid grid-cols-1 md:grid-cols-2 justify-items-center place-items-center'>
-                <div className='bg-blue w-[600px] h-[400px] rounded-[16px] py-[60px] px-[64px] gap-[32px] flex flex-col justify-center items-center text-center'>
-                    <Image width={80} height={80} className='object-cover' src={kite} alt='Object'></Image>
-                    <h1 className='font-instrument text-[24px] text-grayTwentyOne font-semibold'>Innovation</h1>
-                    <p className='leading-[25.2px] font-normal text-[1.125rem] text-grayTwentyTwo'>We push the boundaries of what's possible by continuously developing new, future-focused IT solutions.</p>
+            <div className='w-full gap-[40px] lg:gap-y-[64px] lg:gap-x-[40px] grid grid-cols-1 md:grid-cols-2 justify-items-center place-items-center'>
+                <div className='bg-blue w-[328px] md:w-[95%] xl:w-[600px] h-[320px] md:h-[400px] rounded-[8px] lg:rounded-[16px] py-[56px] lg:py-[60px] px-[16px] xl:px-[64px] lg:gap-[32px] gap-[16px] lg:px] flex flex-col justify-center items-center text-center'>
+                    <Image className='object-cover w-[56px] lg:w-[80px] h-[56px] lg:h-[80px]cover' src={kite} alt='Object'></Image>
+                    <h1 className='font-instrument text-[1.125rem] lg:text-[1.5rem] text-grayTwentyOne font-semibold'>Innovation</h1>
+                    <p className='leading-[19.6px] lg:leading-[25.2px] font-normal lg:text-[1.125rem] text-[.875rem] text-grayTwentyTwo'>We push the boundaries of what's possible by continuously developing new, future-focused IT solutions.</p>
                 </div>
-                <div className='bg-graySeven w-[600px] h-[400px] rounded-[16px] py-[60px] px-[64px] gap-[32px] flex flex-col justify-center items-center text-center border border-grayFifteen'>
-                    <Image width={80} height={80} className='object-cover' src={square} alt='Object'></Image>
-                    <h1 className='font-instrument text-[24px] text-blue font-semibold'>Reliability</h1>
-                    <p className='leading-[25.2px] font-normal text-[1.125rem] text-blue'>Our clients trust us to deliver on time, every time, with an uncompromising commitment to quality and security.</p>
+                <div className='bg-graySeven w-[328px] md:w-[95%] xl:w-[600px] h-[320px] md:h-[400px] rounded-[8px] lg:rounded-[16px] py-[56px] lg:py-[60px] px-[16px] xl:px-[64px] gap-[16px] lg:gap-[32px] flex flex-col justify-center items-center text-center border border-grayFifteen'>
+                    <Image className='object-cover w-[56px] lg:w-[80px] h-[56px] lg:h-[80px]' src={square} alt='Object'></Image>
+                    <h1 className='font-instrument text-[1.125rem] lg:text-[1.5rem] text-blue font-semibold'>Reliability</h1>
+                    <p className='leading-[19.6px] lg:leading-[25.2px] font-normal lg:text-[1.125rem] text-[.875rem] text-blue'>Our clients trust us to deliver on time, every time, with an uncompromising commitment to quality and security.</p>
                 </div>
-                <div className='bg-graySeven w-[600px] h-[400px] rounded-[16px] py-[60px] px-[64px] gap-[32px] flex flex-col justify-center items-center text-center border border-grayFifteen'>
-                    <Image width={80} height={80} className='object-cover' src={circle} alt='Object'></Image>
-                    <h1 className='font-instrument text-[24px] text-greenTwo font-semibold'>Top-Notch Technology</h1>
-                    <p className='leading-[25.2px] font-normal text-[1.125rem] text-blue'>Leveraging the most advanced tools and platforms to build sustainable, high-performance digital ecosystems.</p>
+                <div className='bg-graySeven w-[328px] md:w-[95%] xl:w-[600px] h-[320px] md:h-[400px] rounded-[8px] lg:rounded-[16px] py-[56px] lg:py-[60px] px-[16px] xl:px-[64px] gap-[16px] lg:gap-[32px] flex flex-col justify-center items-center text-center border border-grayFifteen'>
+                    <Image className='object-cover w-[56px] lg:w-[80px] h-[56px] lg:h-[80px]' src={circle} alt='Object'></Image>
+                    <h1 className='font-instrument text-[1.125rem] lg:text-[1.5rem] text-greenTwo font-semibold'>Top-Notch Technology</h1>
+                    <p className='leading-[19.6px] lg:leading-[25.2px] font-normal lg:text-[1.125rem] text-[.875rem] text-blue'>Leveraging the most advanced tools and platforms to build sustainable, high-performance digital ecosystems.</p>
                 </div>
-                <div className='bg-greenTwo w-[600px] h-[400px] rounded-[16px] py-[60px] px-[64px] gap-[32px] flex flex-col justify-center items-center text-center border border-grayFifteen'>
-                    <Image width={80} height={80} className='object-cover' src={wKite} alt='Object'></Image>
-                    <h1 className='font-instrument text-[24px] text-grayTwentyOne font-semibold'>Top-Notch Technology</h1>
-                    <p className='leading-[25.2px] font-instrument font-normal text-[1.125rem] text-grayTwentyTwo'>We are dedicated to helping businesses transform digitally, optimizing operations, and unlocking new growth opportunities.</p>
+                <div className='bg-greenTwo w-[328px] md:w-[95%] xl:w-[600px] h-[320px] md:h-[400px] rounded-[8px] lg:rounded-[16px] py-[56px] lg:py-[60px] px-[16px] xl:px-[64px] gap-[16px] lg:gap-[32px] flex flex-col justify-center items-center text-center border border-grayFifteen'>
+                    <Image className='object-cove w-[56px] lg:w-[80px] h-[56px] lg:h-[80px]r' src={wKite} alt='Object'></Image>
+                    <h1 className='font-instrument text-[1.125rem] lg:text-[1.5rem] text-grayTwentyOne font-semibold'>Top-Notch Technology</h1>
+                    <p className='leading-[19.6px] lg:leading-[25.2px] font-instrument font-normal text-[.875rem] lg:text-[1.125rem] text-grayTwentyTwo'>We are dedicated to helping businesses transform digitally, optimizing operations, and unlocking new growth opportunities.</p>
                 </div>
             </div>
         </div>
